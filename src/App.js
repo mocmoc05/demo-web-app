6e48fcc94b094c6c75f64fbca07007b7c25fb38d
Hoàn Vũ Văn <hoanvv.tb@gmail.com>
2020-03-04 23:38:57 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
 export default class App extends React.Component {
-
   render() {
     return (
       <Provider store={store}>
